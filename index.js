f776931625e9e7941e52ae6d49242243f3af9dd1
Yaremko Ivan <yaremich98@gmail.com>
2021-09-30 11:31:56 +0300

    Add task switch

--- index.js
+++ index.js
@@ -30,3 +30,36 @@
 //     console.log(data ? 'Thank you: '+ data: 'not thank you');
 // }
 // inputData();
+
+
+// const season = prompt ('Enter season\n 1, 2, 12 -winter\n 3, 4, 5 - spring\n 6, 7, 8 - summer\n 9, 10, 11 - fall');
+ 
+//     switch (season) {
+//         case '1':
+//         case '2':
+//         case '12':
+//             console.log('Winter');
+//                 break;
+
+//         case '3':
+//         case '4':
+//         case '5':
+//             console.log('Spring');
+//                 break;  
+                
+//         case '6':
+//         case '7':
+//         case '8':
+//             console.log('Summer');
+//                 break;
+                
+//         case '9':
+//         case '10':
+//         case '11':
+//             console.log('Fall');
+//                 break;
+
+//         default:
+//             console.log('not');
+//                 break;
+//     }
