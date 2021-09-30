00b4bdced840fce13184e84a5d04056735ef5527
Yaremko Ivan <yaremich98@gmail.com>
2021-09-30 12:00:51 +0300

    Add calculate

--- index.js
+++ index.js
@@ -63,3 +63,46 @@
 //             console.log('not');
 //                 break;
 //     }
+
+
+const sum = function(a,b){
+    return a+b;
+}
+
+const sub = function(a,b){
+    return a-b;
+}
+
+const mult = function(a,b){
+    return a*b;
+}
+
+const div = function(a,b){
+    return a*b;
+}
+
+const calculate = function(num1=10, num2=2, operator='+'){
+    if( isNaN(num1-num2) ){
+        return false;
+    }
+    let f;
+    switch (operator) {
+        case '+':
+            f = sum;
+            break;
+        case '-':
+            f = sub;
+            break;
+        case '*':
+            f = mult;
+            break;
+        case '/':
+            f = div;
+            break;
+        default:
+            return null;
+    }
+    return(typeof f==='function')?f(num1,num2):f;
+}
+
+console.log(calculate());
